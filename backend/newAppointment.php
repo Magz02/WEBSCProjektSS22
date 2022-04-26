aae093c1612429ec38ef1d0ef320cc38dc042faf
Jana <100678972+ginger-darklord@users.noreply.github.com>
2022-04-26 15:43:22 +0200

    1-Startseite-different

--- backend/newAppointment.php
+++ backend/newAppointment.php
@@ -4,26 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../frontend/js/script.js"></script>
+
     <title>Create new appointment</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     <?php
         if(isset($_POST['create'])) {
             $creatorName = $_POST['creatorName'];
+            $location = $_POST['location'];
             $appointName = $_POST['appointName'];
             $description = $_POST['description'];
             $appDate = date($_POST['appDate']);
+            $appTime = $_POST['appTime'];
 
             require 'db/db.php';
-            $query = "INSERT INTO appointments (creator, appName, description, date) values (?, ?, ?, ?);";
+            $query = "INSERT INTO appointments (creator, location, appName, description) values (?,?, ?, ?);";
             $statement = $conn->prepare($query);
-            if (!$statement->bind_param("ssss", $creatorName, $appointName, $description, $appDate)){
+            if (!$statement->bind_param("ssss", $creatorName,$location, $appointName, $description)){
                 echo "Binding not working.";
             }
             if (!$statement->execute()) {
                 $error = "The Appointment could not be created. Try again!";
             }
+
+            $query = "INSERT INTO availdates (appName, date, time) values (?, ?, ?);";
+                    $statement = $conn->prepare($query);
+                    if (!$statement->bind_param("sss",$appointName, $appDate, $appTime)){
+                        echo "Binding not working.";
+                    }
+                    if (!$statement->execute()) {
+                        $error = "The Appointment could not be created. Try again!";
+                    }
         }
     ?>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
@@ -32,24 +46,30 @@
                 <a class="nav-link" href="index.html">Main page</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="../backend/newAppointment.php">Create new appointment</a>
+                <a class="nav-link" href="newAppointment.php">Create new appointment</a>
             </li>
         </ul>
     </nav>
     <div>
         <form action="newAppointment.php" method="post">
-            <div class="container" style="background-color: black;">
+            <div class="container" style="background-color: light;">
                 <label for="creatorName">Your name:</label></br>
                 <input type="text" id="creatorName" name="creatorName" placeholder="Jane Doe" required minlength="3"></br>
                 
                 <label for="appointName">Appointment name:</label></br>
                 <input type="text" id="appointName" name="appointName" placeholder="Appointment name" required /></br>
 
+                <label for="location">Location:</label></br>
+                <input type="text" id="location" name="location" placeholder="Location" required minlength="3"></br>
+
                 <label for="description">Description:</label></br>
                 <textarea id="description" name="description" placeholder="Type your description (optional)..."></textarea></br>
-                
+
                 <label for="appDate">Date:</label></br>
-                <input type="date" id="appDate" name="appDate" max="2030-12-31"></br> <!--Min als heutige Datum anlegen-->
+                <input type="date" id="appDate" name="appDate" max="2030-12-31"></br><!--Min als heutige Datum anlegen-->
+
+                <label for="appTime">Time:</label></br>
+                <input type="time" id="appTime" name="appTime"><br>
 
                 <input type="submit" name="create" value="Create the appointment" /></br>
                 <?php
@@ -59,7 +79,7 @@
                     } else if (!isset($error) && isset($_POST["create"])) {
                         echo '<span style="color: yellowgreen">The Appointment was successfully created!</span>';
                         echo '<p>You will be redirected to the main page shortly.</p>';
-                        header("Refresh: 3; url=../frontend/index.html");
+                        header("Refresh: 3; url= ../frontend/index.html");
                     }
                 ?>
             </div>
